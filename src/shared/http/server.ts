1538b11dac5bf1d3d047010ab5b4b82c01d1ded8
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-04-14 20:34:23 -0300

    feat: add handle errors application

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 const port = '3333' || process.env.PORT;
 
@@ -8,6 +9,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  } else {
+    return res.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error!',
+    });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
