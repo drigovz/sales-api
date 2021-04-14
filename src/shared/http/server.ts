d9f8625ac8107f0512f078dbafd905b94490891a
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-04-14 19:52:00 -0300

    feat: initialize application

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const port = '3333' || process.env.PORT;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
