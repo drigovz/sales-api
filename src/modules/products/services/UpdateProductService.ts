2a3834e3ddc3e93eace3065a12c6e90a08515c5c
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-04-19 19:34:42 -0300

    feat: add update product service

--- src/modules/products/services/UpdateProductService.ts
+++ src/modules/products/services/UpdateProductService.ts
@@ -4,16 +4,27 @@ import IRequest from '../interfaces/IRequest';
 import Product from '../typeorm/entities/Product';
 import { ProductsRepository } from '../typeorm/repositories/ProductsRepository';
 
-class ShowProductService {
-  public async execute({ id }: IRequest): Promise<Product> {
+class UpdateProductService {
+  public async execute({ id, name, price, quantity }: IRequest): Promise<Product> {
     const productRepository = getCustomRepository(ProductsRepository);
     const product = await productRepository.findOne(id);
     if (!product) {
       throw new AppError('Product not found!');
     }
 
+    const productExists = await productRepository.findByName(name);
+    if (productExists) {
+      throw new AppError('There is already one product with this name!');
+    }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productRepository.save(product);
+
     return product;
   }
 }
 
-export default ShowProductService;
+export default UpdateProductService;
