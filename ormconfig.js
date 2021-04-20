2fa89ac6ca35f0ac4a8929ce9bd92647c74b352e
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-04-19 21:52:58 -0300

    feat: add path entities on ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -5,6 +5,7 @@ module.exports = {
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
+  entities: ['./src/modules/**/typeorm/entities/*.ts'],
   migrations: ['./src/shared/typeorm/migrations/*.ts'],
   cli: {
     migrationsDir: './src/shared/typeorm/migrations/',
