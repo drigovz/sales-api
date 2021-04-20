acb71e6e3fa12172e6c9f009e33c6d0ca9ed68b3
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-04-19 21:25:07 -0300

    feat: add routes in application

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,7 +1,9 @@
+import productRouter from '@modules/products/routes/products.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
+routes.use('/products', productRouter);
 routes.get('/', (req, res) => {
   return res.json({ message: 'Server is running!' });
 });
