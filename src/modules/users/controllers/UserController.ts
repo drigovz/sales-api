170d8b213f38af557755a2f35f1a658a52fc5ccb
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-04-21 19:33:28 -0300

    feat: finish user controller

--- src/modules/users/controllers/UserController.ts
+++ src/modules/users/controllers/UserController.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from 'express';
+import CreateUserService from '../services/CreateUserService';
+import DeleteUserService from '../services/DeleteUserService';
 import ListUserService from '../services/ListUserService';
 import ShowUserService from '../services/ShowUserService';
 
 class UserController {
   public async index(req: Request, res: Response): Promise<Response> {
     const listUsers = new ListUserService();
-    const users = listUsers.execute();
+    const users = await listUsers.execute();
+
     return res.json(users);
   }
 
@@ -15,6 +18,22 @@ class UserController {
     const user = await showProduct.execute({ id });
 
     return res.json(user);
+  }
+
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { name, email, password } = req.body;
+    const createUser = new CreateUserService();
+    const user = await createUser.execute({ name, email, password });
+
+    return res.json(user);
+  }
+
+  public async delete(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const deleteUser = new DeleteUserService();
+    await deleteUser.execute({ id });
+
+    return res.json({ message: 'User deleted!' });
   }
 }
 
