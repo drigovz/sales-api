a0334c6b85a41ce175b2deb340982c19acea5e2b
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-04-21 21:34:20 -0300

    feat: add cryptography to password users

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,12 @@
 import productRouter from '@modules/products/routes/products.routes';
+import usersRouter from '@modules/users/routes/users.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
 routes.use('/products', productRouter);
+routes.use('/users', usersRouter);
+
 routes.get('/', (req, res) => {
   return res.json({ message: 'Server is running!' });
 });
