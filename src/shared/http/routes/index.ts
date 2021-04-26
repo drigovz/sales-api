51cd65132b492ceacf75ea5bd79e18ac86f324db
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-04-26 20:05:02 -0300

    feat: add session routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,11 +1,13 @@
 import productRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/routes/users.routes';
+import sessionsRouter from '@modules/users/routes/sessions.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
 routes.use('/products', productRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Server is running!' });
