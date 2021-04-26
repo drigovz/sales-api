a041a074f62ce5b863141f166d0ab8cf657fb8ab
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-04-26 19:39:52 -0300

    feat: init auth token configuration

---
+++ src/modules/users/services/CreateSessionService.ts
@@ -0,0 +1,33 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import { compare, hash } from 'bcryptjs';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: User;
+}
+
+class CreateSessionService {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const user = await usersRepository.findByEmail(email);
+    if (!user) {
+      throw new AppError('Incorrect email/password combination!', 401);
+    }
+
+    const passwordConfirmed = await compare(password, user.password);
+    if (!passwordConfirmed) {
+      throw new AppError('Incorrect password combination!', 401);
+    }
+
+    return user;
+  }
+}
+
+export default CreateSessionService;
