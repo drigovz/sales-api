60e769dd6aff6f7e5a6c083907d9dc14aaa081a7
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-05-13 19:40:20 -0300

    feat: finished custom email template service

--- src/config/mail/HandlebarsMailTemplate.ts
+++ src/config/mail/HandlebarsMailTemplate.ts
@@ -1,4 +1,5 @@
 import handlebars from 'handlebars';
+import fs from 'fs';
 
 interface ITemplateVariables {
   [key: string]: string | number;
@@ -11,7 +12,12 @@ interface IParserMailTemplate {
 
 class HandlebarsMailTemplate {
   public async parser({ template, variables }: IParserMailTemplate): Promise<string> {
-    const parseTemplate = handlebars.compile(template);
+    const templateFileContent = await fs.promises.readFile(template, {
+      encoding: 'utf-8',
+    });
+
+    const parseTemplate = handlebars.compile(templateFileContent);
+
     return parseTemplate(variables);
   }
 }
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from 'typeorm';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
 import EtherealMail from '@config/mail/EtherealMail';
+import path from 'path';
 
 interface IRequest {
   email: string;
@@ -20,6 +21,8 @@ class SendForgotPasswordEmailService {
 
     const { token } = await userTokenRepository.generate(user.id);
 
+    const forgotPasswordTemplate = path.resolve(__dirname, '..', 'views', 'forgot_password.hbs');
+
     await EtherealMail.sendMail({
       to: {
         name: user.name,
@@ -27,10 +30,10 @@ class SendForgotPasswordEmailService {
       },
       subject: '[Sales API] Password recovery!',
       templateData: {
-        template: 'Hello {{name}}! your request password definition received: {{token}}',
+        template: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token,
+          link: `http://localhost:3333/reset_password?token=${token}`,
         },
       },
     });
