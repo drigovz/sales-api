3777ab3fc2d8a9c32968fa16d1aae11f26f4c6c6
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-05-13 21:17:36 -0300

    feat: add update user profile controller

---
+++ src/modules/users/controllers/ProfileControler.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import ShowProfileService from '../services/ShowProfileService';
+import UpdateProfileService from '../services/UpdateProfileService';
+
+class ProfileController {
+  public async show(req: Request, res: Response): Promise<Response> {
+    const showProfile = new ShowProfileService();
+    const id = req.user.id;
+
+    const user = await showProfile.execute({ id });
+
+    return res.json(user);
+  }
+
+  public async update(req: Request, res: Response): Promise<Response> {
+    const id = req.user.id;
+    const { name, email, password, old_password } = req.body;
+
+    const updateProfile = new UpdateProfileService();
+
+    const user = updateProfile.execute({ id, name, email, password, old_password });
+
+    return res.json(user);
+  }
+}
+
+export default ProfileController;
