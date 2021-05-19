1980736d2c99cb39ef64a924fd76c5f21bbeaa95
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-05-19 16:20:26 -0300

    feat: add CreateOrderService

---
+++ src/modules/orders/services/CreateOrderService.ts
@@ -0,0 +1,76 @@
+import AppError from '@shared/errors/AppError';
+import Order from '../typeorm/entities/Order';
+import { getCustomRepository } from 'typeorm';
+import { ProductsRepository } from '@modules/products/typeorm/repositories/ProductsRepository';
+import CustomersRepository from '@modules/customers/typeorm/repositories/CustomersRepository';
+import OrdersRepository from '../typeorm/repositories/OrdersRepository';
+
+interface IProduct {
+  id: string;
+  price: number;
+  quantity: number;
+}
+
+interface IRequest {
+  customer_id: string;
+  products: IProduct[];
+}
+
+class CreateOrderService {
+  public async execute({ customer_id, products }: IRequest): Promise<Order> {
+    const ordersRepository = getCustomRepository(OrdersRepository);
+    const customersRepository = getCustomRepository(CustomersRepository);
+    const productsRepository = getCustomRepository(ProductsRepository);
+
+    const customerExists = await customersRepository.findById(customer_id);
+    if (!customerExists) {
+      throw new AppError('Could not find any customer with given id!');
+    }
+
+    const existsProducts = await productsRepository.findAllByIds(products);
+    if (!existsProducts.length) {
+      throw new AppError('Could not find any products with given ids!');
+    }
+
+    // verify if products really exists in database
+    // get all products ids
+    const existsProductsIds = existsProducts.map(product => product.id);
+    // get all inexistent products ids
+    const checkInexistentProducts = products.filter(product => !existsProductsIds.includes(product.id));
+    // if 'checkInexistentProducts' is biger than zero, so have inexistents products ids on this list
+    if (checkInexistentProducts.length) {
+      throw new AppError(`Could not find product id: ${checkInexistentProducts[0].id}!`);
+    }
+
+    // verify if quantity is valid
+    const quantityAvaliable = products.filter(product => existsProducts.filter(p => p.id === product.id)[0].quantity < product.quantity);
+    if (quantityAvaliable.length) {
+      throw new AppError(`The quantity ${quantityAvaliable[0].quantity} is not avaliable for ${quantityAvaliable[0].id}!`);
+    }
+
+    const serializedProducts = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: existsProducts.filter(p => p.id === product.id)[0].price,
+    }));
+
+    const order = await ordersRepository.createOrder({
+      customer: customerExists,
+      products: serializedProducts,
+    });
+
+    const { order_products } = order;
+
+    // update quantity of products
+    const updatedProductQuantity = order_products.map(product => ({
+      id: product.id,
+      quantity: existsProducts.filter(p => p.id === product.id)[0].quantity - product.quantity,
+    }));
+
+    await productsRepository.save(updatedProductQuantity);
+
+    return order;
+  }
+}
+
+export default CreateOrderService;
