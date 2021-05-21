497b12bb694fe28f8530c69caf45c7667971f9ab
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-05-20 21:54:10 -0300

    feat: config docker-compose to Redis

---
+++ createdir.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+userLogged=$USER
+
+if [ -d "/home/$USER/Dev/redisdb" ]
+then
+    echo "Directory /home/drigolx/Dev/redisdb exists."
+    sudo chown -R $userLogged /home/$userLogged/Dev/redisdb
+else
+    echo "Creating directory... "
+    mkdir /home/$userLogged/Dev/redisdb
+    sudo chown -R $userLogged /home/$userLogged/Dev/redisdb
+fi
