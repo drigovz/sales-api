332e712b540586d8b7a31236a59231ed74f46917
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-05-22 15:33:19 -0300

    feat: init configuration of structure of class redis caches

---
+++ src/shared/cache/RedisCache.ts
@@ -0,0 +1,20 @@
+import Redis, { Redis as RedisClient } from 'ioredis';
+import cacheConfig from '@config/cache';
+
+class RedisCache {
+  private client: RedisClient;
+
+  constructor() {
+    this.client = new Redis(cacheConfig.config.redis);
+  }
+
+  public async save(key: string, value: any): Promise<void> {
+    console.log(key, value);
+  }
+
+  // public async recover<T>(key: string): Promise<T | null> {}
+
+  // public async invalidate(key: string): Promise<void> {}
+}
+
+export default RedisCache;
