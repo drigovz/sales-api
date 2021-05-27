c01fe75b5cc8a6a89a493cde175fab66d979e42f
Rodrigo Vaz <rodrigodp2014@gmail.com>
2021-05-26 21:49:48 -0300

    refactor: init refactor

--- ormconfig.js
+++ ormconfig.js
@@ -5,9 +5,9 @@ module.exports = {
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
-  entities: ['./src/modules/**/typeorm/entities/*.ts'],
-  migrations: ['./src/shared/typeorm/migrations/*.ts'],
+  entities: ['./src/modules/**/infra/typeorm/entities/*.ts'],
+  migrations: ['./src/shared/infra/typeorm/migrations/*.ts'],
   cli: {
-    migrationsDir: './src/shared/typeorm/migrations/',
+    migrationsDir: './src/shared/infra/typeorm/migrations/',
   },
 };
